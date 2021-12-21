343f39ee16e78ebad8f60d6bffcbbdf2134bc97a
CIAIP21APR3001 <sarthakgupta550@gmail.com>
2021-12-22 00:13:33 +0530

    2. Transfer Tokens

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,9 +1,35 @@
 pragma solidity ^0.5.0;
 
 contract MyToken {
+    string public name = "My Token";
+    string public symbol = "MT";
+    string public standard = "My Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate the initial supply
+    }
+
+    // Transfer
+    // Exception if account doesn't have enough balance
+    // Return boolean
+    // Transfer Event
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const MyToken = artifacts.require("./MyToken.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(MyToken);
+    deployer.deploy(MyToken, 1000000).then(function () {
+
+        // Token price is 0.001 Ether
+        var tokenPrice = 1000000000000000;
+    });
+
+
 };
