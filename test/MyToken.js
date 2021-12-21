c2f2ade21775f4ca673ea95f4a7b2bf1f6a20f83
CIAIP21APR3001 <sarthakgupta550@gmail.com>
2021-12-21 15:54:19 +0530

    1. Smoke Test

---
+++ test/MyToken.js
@@ -0,0 +1,12 @@
+var MyToken = artifacts.require("./MyToken.sol");
+
+contract('MyToken', function (accounts) {
+    it('sets the total supply upon deployment', function () {
+        return MyToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        }).then(function (totalSupply) {
+            assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+        });
+    });
+})
